bb7ec446c6712c08bb4f2640212f744e531e53aa
pianohagens <pianohagens@gmail.com>
2016-02-17 12:31:57 -0800

    commitLoginRegistrationService_ShowTracker-WorkingOne

---
+++ App_Code/IService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
+[ServiceContract]
+public interface IService
+{
+    [OperationContract]
+    int venueLogin(string password, string username);
+
+    [OperationContract]
+    int venueRegistration(VenueLogin l);
+
+}
+
+[DataContract]
+public class venueLite
+{
+   
+    [DataMember]
+    public string venueName { set; get; }
+
+    [DataMember]
+    public string venueAddress { set; get; }
+
+    [DataMember]
+    public string venueCity { set; get; }
+
+    [DataMember]
+    public string venueState { set; get; }
+
+    [DataMember]
+    public string venueZipCode { set; get; }
+
+    [DataMember]
+    public string venuePhone { set; get; }
+
+    [DataMember]
+    public string venueEmail { set; get; }
+
+    [DataMember]
+    public string venueWebPage { set; get; }
+
+    [DataMember]
+    public int venueAgeRestriction { set; get; }
+
+    [DataMember]
+    public string venueLoginUserName { set; get; }
+
+    [DataMember]
+    public string venueLoginPasswordPlain { set; get; }
+
+
+
+}
---
+++ App_Code/Service.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+    ShowTrackerEntities db = new ShowTrackerEntities();
+    public int venueLogin(string password, string username)
+    {
+        int result = db.usp_venueLogin(username, password);
+        if (result != -1)
+        {
+            var key = from k in db.Venues
+                      where k.VenueName.Equals(username)
+                      select new { k.VenueKey };
+            foreach (var k in key)
+            {
+                result = (int)k.VenueKey;
+            }
+        }
+
+        return result;
+    }
+
+    public int venueRegistration(venueLite l)
+    {
+
+        int result = db.usp_RegisterVenue(l.venueName, 
+            l.venueAddress, l.venueCity, l.venueState, l.venueZipCode, l.venuePhone, 
+            l.venueEmail, l.venueWebPage, 
+            l.venueAgeRestriction, l.venueLoginUserName, l.venueLoginPasswordPlain);
+
+        return result;
+    }
+
+
+    public int venueRegistration(VenueLogin l)
+    {
+        throw new NotImplementedException();
+    }
+}
